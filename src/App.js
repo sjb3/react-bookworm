fd36e7562200a0c06bbde860c3194fde7297c28f
Sung J Byun <sungjbyun@D-173-250-236-145.dhcp4.washington.edu>
2017-09-04 17:35:04 -0700

    basic route set up with corresponding pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 // class App extends Component {
 //   render() {
@@ -16,6 +19,9 @@ import React from 'react';
 //   }
 // }
 
-const App = () => <div>Hello World</div>
+const App = () => <div>
+  <Route path="/" exact component={ HomePage } />
+  <Route path="/login" exact component={ LoginPage } />
+</div>
 
 export default App;
