ffc26a274b615a7ce77c276fc058a69c56772f3c
Sung J Byun <sungjbyun@D-10-72-117-7.dhcp4.washington.edu>
2018-01-02 14:34:32 -0800

    fixed ConfirmationPage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
-// import SignupPage from './components/pages/SignupPage';
+import SignupPage from './components/pages/SignupPage';
+import ConfirmationPage from './components/pages/ConfirmationPage';
 import DashboardPage from './components/pages/DashboardPage';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
@@ -12,7 +13,9 @@ import GuestRoute from './components/routes/GuestRoute';
 const App = ({ location }) => (
   <div className='ui container'>
     <Route location={location} path='/' exact component = {HomePage} />
+    <Route location={location} path='/confirmation/:token' exact component = {ConfirmationPage} />
     <GuestRoute location={location} path='/login' exact component = {LoginPage} />
+    <GuestRoute location={location} path='/singup' exact component = {SignupPage} />
     <UserRoute location={location} path='/dashboard' exact component = {DashboardPage} />
   </div>
 );
